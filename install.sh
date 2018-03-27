c1a717ed26478dc9c620f45040abf379f4bb3db8
C.D. Clark III <clifton.clark@gmail.com>
2018-03-27 09:16:40 -0500

    fixed install script. no longer edits .bashrc since this doens't affect scripts.

--- install.sh
+++ install.sh
@@ -3,29 +3,10 @@
 source import_utils
 set -e
 
-mkdir -p ${IMPORT_MODULE_PATH}
-install_dir=$(basename ${IMPORT_MODULE_PATH})
+install_dir=$(dirname ${IMPORT_MODULE_PATH})
 mkdir -p ${install_dir}
 cp import_utils ${install_dir}
+mkdir -p ${IMPORT_MODULE_PATH}
 
-
-bashrc=${HOME}/.bashrc
-edit_bashrc=0
-
-if [[ ! -e $bashrc ]]
-then
-  edit_bashrc=1
-fi
-
-if grep "import_utils" $bashrc > /dev/null
-then
-  echo "import_utils found in $bashrc. will not edit."
-else
-  echo "import_utils not found in $bashrc. will edit."
-  edit_bashrc=1
-fi
-
-if [[ $edit_bashrc -eq 1 ]]
-then
-  echo "source ${install_dir}/import_utils" >> ${bashrc}
-fi
+echo "'import_utils' was installed to '${install_dir}'"
+echo "To use it, add 'source ${install_dir}/import_utils' to your script"
